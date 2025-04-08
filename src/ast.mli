71ab43fb498f95fdc2b86403fcae6587fa20833c
Kristófer Fannar Björnsson <68929760+kristoferfannar@users.noreply.github.com>
2025-04-08 17:59:12 -0400

    setup format rules and workflow (#7)
    * setup format rules and workflow
    
    * move directories (oops)
    
    * install ocamlformat
    
    * `ocamlformat`
    
    * format again
    
    ---------
    
    Co-authored-by: xytuang <88203777+xytuang@users.noreply.github.com>

--- src/ast.mli
+++ src/ast.mli
@@ -1,3 +1,1 @@
-
-type program = 
-    Program
+type program = Program
--- src/fly.ml
+++ src/fly.ml
@@ -1,31 +1,31 @@
 (* open Printf
 
 
-open Parser 
 
-let get_token_list lexbuf = 
-    let rec work acc = 
-        match Scanner.tokenize lexbuf with 
-        | EOF -> acc
-        | t -> work (t :: acc)
-    in List.rev (work [])
+let get_token_list lexbuf =
+  let rec work acc =
+    match Scanner.tokenize lexbuf with
+    | EOF -> acc
+    | t -> work (t :: acc)
+  in
+  List.rev (work [])
+;;
 
 let pp_token = function
-    | EQ -> "="
-    | DIVIDE -> "/"
-    | LITERAL(i) -> sprintf "%d" i
-    | ID(v) -> sprintf ":%s:" v
-    | SEMI -> ";"
-    | PLUS -> "+"
-    | MINUS -> "-"
-    | TIMES -> "*"
-    | EOF -> "EOF"
-    | _ -> "??"
-
+  | EQ -> "="
+  | DIVIDE -> "/"
+  | LITERAL i -> sprintf "%d" i
+  | ID v -> sprintf ":%s:" v
+  | SEMI -> ";"
+  | PLUS -> "+"
+  | MINUS -> "-"
+  | TIMES -> "*"
+  | EOF -> "EOF"
+  | _ -> "??"
+;;
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-
   (* Debug Tokens *)
   let token_list = get_token_list lexbuf in
   List.map pp_token token_list |> List.iter (printf "%s\n") *)
