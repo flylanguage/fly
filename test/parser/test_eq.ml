ede8abbded43cbca04ee6a8a1536d0bb9a96c79d
Xiang Yu Tuang <xiangyutuang@gmail.com>
2025-04-16 19:10:15 -0400

    more push

--- test/parser/test_eq.ml
+++ test/parser/test_eq.ml
@@ -10,7 +10,8 @@ let tests =
             Lexing.from_string
               "let a1 := 5;\nlet b1 := 5;\nif (a1 == b1) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program) in
+          let program = Parser.program_rule Scanner.tokenize lexbuf 
+          let actual = string_of_program program in
           let expected =
             "let a1 := 5;\nlet b1 := 5;\nif (a1 == b1) {\n} else {\n}\n"
           in
@@ -22,7 +23,8 @@ let tests =
             Lexing.from_string
               "let c1 := 3.14;\nlet d1 := 3.14;\nif (c1 == d1) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program) in
+          let program = Parser.program_rule Scanner.tokenize lexbuf in 
+          let actual = string_of_program program in
           let expected =
              "let c1 := 3.14;\nlet d1 := 3.14;\nif (c1 == d1) {\n} else {\n}\n"
           in
@@ -34,7 +36,8 @@ let tests =
             Lexing.from_string
               "let e1 := \"Hello\";\nlet f1 := \"Hello\";\nif (e1 == f1) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let program = Parser.program_rule Scanner.tokenize lexbuf in 
+          let actual = string_of_program program in
           let expected =
             "let e1 := \"Hello\";\nlet f1 := \"Hello\";\nif (e1 == f1) {\n} else {\n}\n"
           in
@@ -46,7 +49,7 @@ let tests =
             Lexing.from_string
               "let g1 := true;\nlet h1 := true;\nif (g1 == h1) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
             "let g1 := true;\nlet h1 := true;\nif (g1 == h1) {\n} else {\n}\n"
           in
@@ -58,7 +61,7 @@ let tests =
             Lexing.from_string
               "let a2 := 5;\nlet b2 := 10;\nif (a2 == b2) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
               "let a2 := 5;\nlet b2 := 10;\nif (a2 == b2) {\n} else {\n}\n"
           in
@@ -70,7 +73,7 @@ let tests =
             Lexing.from_string
               "let c2 := 3.14;\nlet d2 := 2.71;\nif (c2 == d2) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
             "let c2 := 3.14;\nlet d2 := 2.71;\nif (c2 == d2) {\n} else {\n}\n"
           in
@@ -82,7 +85,7 @@ let tests =
             Lexing.from_string
               "let e2 := \"Hello\";\nlet f2 := \"World\";\nif (e2 == f2) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
             "let e2 := \"Hello\";\nlet f2 := \"World\";\nif (e2 == f2) {\n} else {\n}\n"
           in
@@ -94,7 +97,7 @@ let tests =
             Lexing.from_string
               "let g2 := true;\nlet h2 := false;\nif (g2 == h2) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
             "let g2 := true;\nlet h2 := false;\nif (g2 == h2) {\n} else {\n}\n"
           in
@@ -106,7 +109,7 @@ let tests =
             Lexing.from_string
               "let a3 := 5;\nlet b3 := \"5\";\nif (a3 == b3) {\n} else {\n}\n"
           in
-          let actual = Parser.program Scanner.tokenize lexbuf in print_endline (string_of_program program)
+          let actual = Parser.program_rule Scanner.tokenize lexbuf in print_endline (string_of_program program)
           let expected =
             "let a3 := 5;\nlet b3 := \"5\";\nif (a3 == b3) {\n} else {\n}\n"
           in
