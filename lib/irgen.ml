29dbe1b65a09e337bb3dce610f4b0a460b8d1b90
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-04-29 17:06:19 -0400

    improve ir structure

--- lib/irgen.ml
+++ lib/irgen.ml
@@ -1,9 +1,21 @@
 module L = Llvm
 module A = Ast
+module S = Sast
 
-(* let translate (globals, functions) = *)
-let translate  =
+let translate blocks =
   let context = L.global_context () in
   let the_module = L.create_module context "Fly" in
+  let process_block block =
+    match block with
+    | S.SMutDeclTyped (_s, _t, _e) -> print_endline "SMutDeclTyped found"
+    | _ -> raise (Failure "not implemented")
+  in
+  let rec process_blocks = function
+    | [] -> ()
+    | block :: rest ->
+      process_block block;
+      process_blocks rest
+  in
+  process_blocks blocks;
   the_module
 ;;
--- test/ir/test_ir.ml
+++ test/ir/test_ir.ml
@@ -1,13 +1,37 @@
 open OUnit2
 open Fly_lib
-(* (* open Print_lib.Prints *) *)
 module L = Llvm
+
+let get_sast input =
+  try
+    let lexbuf = Lexing.from_string input in
+    let ast = Fly_lib.Parser.program_rule Fly_lib.Scanner.tokenize lexbuf in
+    let sast = Fly_lib.Semant.check ast.body in
+    sast
+  with
+  | err ->
+    raise
+      (Failure
+         (Printf.sprintf
+            "Error generating sast, is your program correct?: error=%s"
+            (Printexc.to_string err)))
+;;
 
 let tests =
   "testing_ir"
-  >::: [ ("test1"
+  >::: [ ("empty_program"
           >:: fun _ ->
-          let mdl = Irgen.translate in
+          let sast = get_sast "" in
+          let mdl = Irgen.translate sast in
+          let actual = L.string_of_llmodule mdl in
+          let expected = "; ModuleID = 'Fly'\nsource_filename = \"Fly\"\n" in
+          (* let _ = Printf.printf "\nhave:\n%s\n" actual in *)
+          (* let _ = Printf.printf "\nwant:\n%s\n" expected in *)
+          assert_equal expected actual)
+       ; ("test1"
+          >:: fun _ ->
+          let sast = get_sast "let a := 5;" in
+          let mdl = Irgen.translate sast in
           let actual = L.string_of_llmodule mdl in
           let expected = "; ModuleID = 'Fly'\nsource_filename = \"Fly\"\n" in
           (* let _ = Printf.printf "\nhave:\n%s\n" actual in *)
