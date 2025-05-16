fbd9aa75128a58a471162cd25e1eb8cc696e0f3a
Kristófer Fannar Björnsson <68929760+kristoferfannar@users.noreply.github.com>
2025-05-16 12:10:00 -0400

    Lists (#42)
    * local int list
    
    * bool list test
    
    * add string list
    
    * fix bugs

---
+++ test/ir/test_list.ml
@@ -0,0 +1,104 @@
+open OUnit2
+open Fly_lib
+module L = Llvm
+
+let get_sast input =
+  try
+    let lexbuf = Lexing.from_string input in
+    let ast = Fly_lib.Parser.program_rule Fly_lib.Scanner.tokenize lexbuf in
+    let sast = Fly_lib.Semant.check ast.body in
+    sast
+  with
+  | err ->
+    raise
+      (Failure
+         (Printf.sprintf
+            "Error generating sast, is your program correct?: error=%s"
+            (Printexc.to_string err)))
+;;
+
+let printer = fun s -> "\n---\n" ^ s ^ "\n---\n"
+
+let _write_to_file text filename =
+  let channel = open_out filename in
+  Printf.fprintf channel "%s" text;
+  close_out channel
+;;
+
+let tests =
+  "testing_lists"
+  >::: [ ("local_list"
+          >:: fun _ ->
+          let sast = get_sast "fun function() -> () {let a := [10, 20, 30];}" in
+          let mdl = Irgen.translate sast in
+          let actual = L.string_of_llmodule mdl in
+          let expected =
+            "; ModuleID = 'Fly'\n\
+             source_filename = \"Fly\"\n\n\
+             define void @function() {\n\
+             entry:\n\
+            \  %list = alloca i32, i32 3, align 4\n\
+            \  %index = getelementptr inbounds i32, i32* %list, i32 0\n\
+            \  store i32 10, i32* %index, align 4\n\
+            \  %index1 = getelementptr inbounds i32, i32* %list, i32 1\n\
+            \  store i32 20, i32* %index1, align 4\n\
+            \  %index2 = getelementptr inbounds i32, i32* %list, i32 2\n\
+            \  store i32 30, i32* %index2, align 4\n\
+            \  %a = alloca i32*, align 8\n\
+            \  store i32* %list, i32** %a, align 8\n\
+            \  ret void\n\
+             }\n"
+          in
+          assert_equal expected actual ~printer)
+       ; ("local_bool_list"
+          >:: fun _ ->
+          let sast = get_sast "fun function() -> () {let a := [true, false];}" in
+          let mdl = Irgen.translate sast in
+          let actual = L.string_of_llmodule mdl in
+          let expected =
+            "; ModuleID = 'Fly'\n\
+             source_filename = \"Fly\"\n\n\
+             define void @function() {\n\
+             entry:\n\
+            \  %list = alloca i1, i32 2, align 1\n\
+            \  %index = getelementptr inbounds i1, i1* %list, i32 0\n\
+            \  store i1 true, i1* %index, align 1\n\
+            \  %index1 = getelementptr inbounds i1, i1* %list, i32 1\n\
+            \  store i1 false, i1* %index1, align 1\n\
+            \  %a = alloca i1*, align 8\n\
+            \  store i1* %list, i1** %a, align 8\n\
+            \  ret void\n\
+             }\n"
+          in
+          _write_to_file actual "actual.out";
+          assert_equal expected actual ~printer)
+       ; ("local_string_list"
+          >:: fun _ ->
+          let sast = get_sast "fun function() -> () {let a := [\"hello\", \"world\"];}" in
+          let mdl = Irgen.translate sast in
+          let actual = L.string_of_llmodule mdl in
+          let expected =
+            "; ModuleID = 'Fly'\n\
+             source_filename = \"Fly\"\n\n\
+             @str = private unnamed_addr constant [6 x i8] c\"hello\\00\", align 1\n\
+             @str.1 = private unnamed_addr constant [6 x i8] c\"world\\00\", align 1\n\n\
+             define void @function() {\n\
+             entry:\n\
+            \  %list = alloca i8*, i32 2, align 8\n\
+            \  %index = getelementptr inbounds i8*, i8** %list, i32 0\n\
+            \  store i8* getelementptr inbounds ([6 x i8], [6 x i8]* @str, i32 0, i32 \
+             0), i8** %index, align 8\n\
+            \  %index1 = getelementptr inbounds i8*, i8** %list, i32 1\n\
+            \  store i8* getelementptr inbounds ([6 x i8], [6 x i8]* @str.1, i32 0, i32 \
+             0), i8** %index1, align 8\n\
+            \  %a = alloca i8**, align 8\n\
+            \  store i8** %list, i8*** %a, align 8\n\
+            \  ret void\n\
+             }\n"
+          in
+          (* _write_to_file actual "actual.out"; *)
+          assert_equal expected actual ~printer)
+       ]
+;;
+
+let _ = run_test_tt_main tests
