a0b6828712ddecefd383477ac4211ab591e84b9b
Xiang Yu Tuang <xiangyutuang@gmail.com>
2025-05-16 01:47:00 -0400

    formatting

--- test/type_checker/test_function.ml
+++ test/type_checker/test_function.ml
@@ -18,23 +18,35 @@ let tests =
           let actual = check_program "fun main() -> int { return 0; }" in
           let expected = "" in
           assert_equal expected actual ~printer:(fun s -> "\"" ^ s ^ "\""))
-        ; ("recursive_function"
+       ; ("recursive_function"
           >:: fun _ ->
-          let actual = check_program "fun function(n: int) -> int { if (n == 0) { return 1; } return function(n - 1); } fun main() -> int { return function(5); }" in
+          let actual =
+            check_program
+              "fun function(n: int) -> int { if (n == 0) { return 1; } return function(n \
+               - 1); } fun main() -> int { return function(5); }"
+          in
           let expected = "" in
           assert_equal expected actual ~printer:(fun s -> "\"" ^ s ^ "\""))
-        
        ; ("call_nested_function"
           >:: fun _ ->
-          let actual = check_program "fun function() -> int { fun nested() -> () { return; } return 0; } fun main() -> int { return nested(); }" in
+          let actual =
+            check_program
+              "fun function() -> int { fun nested() -> () { return; } return 0; } fun \
+               main() -> int { return nested(); }"
+          in
           let expected = "Undefined function nested" in
           assert_equal expected actual ~printer:(fun s -> "\"" ^ s ^ "\""))
-
-      ; ("more_recursion"
+       ; ("more_recursion"
           >:: fun _ ->
-          let actual = check_program "fun outer(m: int) -> int { fun nested(n: int) -> int { if (n == 0) {return 1;} return 1 + nested(n - 1); } return nested(m); } fun main() -> int { return outer(5); }" in
+          let actual =
+            check_program
+              "fun outer(m: int) -> int { fun nested(n: int) -> int { if (n == 0) \
+               {return 1;} return 1 + nested(n - 1); } return nested(m); } fun main() -> \
+               int { return outer(5); }"
+          in
           let expected = "" in
-          assert_equal expected actual ~printer:(fun s -> "\"" ^ s ^ "\""))]
+          assert_equal expected actual ~printer:(fun s -> "\"" ^ s ^ "\""))
+       ]
 ;;
 
 let _ = run_test_tt_main tests
